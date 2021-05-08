147a00a53508b172d4727c3942155cb4becc5e92
vaibhav-barhate95 <83824694+vaibhav-barhate95@users.noreply.github.com>
2021-05-08 16:28:25 +0530

    Update program_23.java

--- program_23.java
+++ program_23.java
@@ -1,49 +1,38 @@
-import java.util.Scanner; 
-import java.lang.Math;
- class Program_23
+import java.util.Scanner;
+public class Que23
 {
-		public static void main(String[] args){
-			int index=0;
-		 int decimal_num = 0;
-		 int reminder=0;
-		 int j=1;
-		int arr[] = new int[50];
-			Scanner scan = new Scanner(System.in);
-			System.out.println("Enter number");
-			int num = scan.nextInt();
-               			while(num>0)
-				{
- 					reminder = num%10;
-					num /=10;
-					decimal_num+=(num%10*j);
-					j *= 2;
-				}
-				j=0;
-				while(decimal_num!=0)
-				{
-					arr[j] = decimal_num % 16;
-					decimal_num /= 16;
-					j++; 	
-				}	
-				/*String hexNum = Integer.toHexString(decimal_num);
-				hexNum = hexNum.toUpperCase();
-					System.out.println("Decimal number is "+hexNum);
-	*/
-				for(int m=j-1;i>=0;i++);
-				{
-					if(arr[m]>9)
-					{
-						System.out.println((char)arr[m]+55);
-			
-					} else
-					{
-						System.out.println(arr[m]);
-					}
-				}
-
-
-
- 	}
-	
+public static void main(String args[])
+{
+int hex[]= new int[100];
+int i=1, j=0, rem, dec=0, bin;
+Scanner sc= new Scanner(System.in);
+System.out.print("Binary No:");
+bin = sc.nextInt();
+while(bin>0)
+{
+rem = bin % 2;
+dec = dec + rem * i;
+i = i*2;
+bin = bin / 8;
+ }
+i = 0;
+while(dec != 0)
+{
+hex[i] = dec % 8;
+dec = dec / 8;
+i++;
 }
-
+System.out.print("Hexadecimnal Value :");
+for(j = i-1; j >= 0; j--)
+{
+if(hex[j] > 9)
+{
+System.out.print((char)(hex[j] + 55)+"\n");
+}
+else
+{
+System.out.print(hex[j]+"\n");
+}
+}
+}
+}
